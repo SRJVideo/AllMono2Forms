9b0e817e1c969038045775fd114a4de903b600fd
Martin Simoneau <martin.simoneau@cegeplimoilou.ca>
2023-07-11 14:46:07 -0400

    Exercice-0-f ajout de la classe BootStrap

---
+++ src/main/java/a23/climoilou/mono2/formatifs/formatif2b/BootStrap.java
@@ -0,0 +1,10 @@
+package a23.climoilou.mono2.formatifs.formatif2b;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class BootStrap implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
