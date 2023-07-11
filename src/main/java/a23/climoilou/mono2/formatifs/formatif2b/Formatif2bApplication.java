04e211ae6dc10ab061dca810ac9233da2c1a3084
Martin Simoneau <martin.simoneau@cegeplimoilou.ca>
2023-07-11 10:38:46 -0400

    Exercice-0 création du projet initial

---
+++ src/main/java/a23/climoilou/mono2/formatifs/formatif2b/Formatif2bApplication.java
@@ -0,0 +1,13 @@
+package a23.climoilou.mono2.formatifs.formatif2b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Formatif2bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Formatif2bApplication.class, args);
+	}
+
+}
---
+++ src/test/java/a23/climoilou/mono2/formatifs/formatif2b/Formatif2bApplicationTests.java
@@ -0,0 +1,13 @@
+package a23.climoilou.mono2.formatifs.formatif2b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Formatif2bApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
