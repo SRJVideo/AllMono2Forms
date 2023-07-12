4a50baae01c61831c26876d6d01888b2848e7c8a
Martin Simoneau <martin.simoneau@cegeplimoilou.ca>
2023-07-12 12:11:18 -0400

    Exercice-1-l utilisation de primary sur Jongleur. BootStrap fonctionne.

--- src/main/java/a23/climoilou/mono2/formatifs/formatif2b/Clown.java
+++ src/main/java/a23/climoilou/mono2/formatifs/formatif2b/Clown.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
-@Primary
 @Component
 public class Clown implements ArtisteI {
 
--- src/main/java/a23/climoilou/mono2/formatifs/formatif2b/Jongleur.java
+++ src/main/java/a23/climoilou/mono2/formatifs/formatif2b/Jongleur.java
@@ -1,9 +1,11 @@
 package a23.climoilou.mono2.formatifs.formatif2b;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
+@Primary
 @Component
 public class Jongleur implements ArtisteI {
     public String performe(LocalDateTime time) {
