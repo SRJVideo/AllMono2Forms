776e63d62e4a1b71486d20f1f9f18dbedb30c4fe
Martin Simoneau <martin.simoneau@cegeplimoilou.ca>
2023-07-16 11:52:06 -0400

    Exercice-3-b-iii  Avec @ComponentScan: on vérifie et le composant Dresseur est découvert par le Contexte

--- controleur/src/main/java/a23/climoilou/mono2/formatifs/formatif2b/Formatif2bApplication.java
+++ controleur/src/main/java/a23/climoilou/mono2/formatifs/formatif2b/Formatif2bApplication.java
@@ -2,12 +2,18 @@ package a23.climoilou.mono2.formatifs.formatif2b;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+
+@ComponentScan("a23.climoilou.mono2.formatifs")  //on scan le parent des 2 dossiers
+//ou on indique chaque dossier précisément:
+@ComponentScan(basePackages = {"a23.climoilou.mono2.formatifs.formatif2b", "a23.climoilou.mono2.formatifs.artistesSupplementaires"})
+
 public class Formatif2bApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Formatif2bApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Formatif2bApplication.class, args);
+    }
 
 }
