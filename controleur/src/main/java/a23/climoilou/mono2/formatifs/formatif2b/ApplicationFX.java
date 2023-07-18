6e4311bb83bd3f199f79a910b20f7ddd4185597e
Martin Simoneau <martin.simoneau@cegeplimoilou.ca>
2023-07-18 08:34:14 -0400

    Exercice-4-b-v  Connecter JavaFx et SpringFramework.

--- controleur/src/main/java/a23/climoilou/mono2/formatifs/formatif2b/ApplicationFX.java
+++ controleur/src/main/java/a23/climoilou/mono2/formatifs/formatif2b/ApplicationFX.java
@@ -8,6 +8,26 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 public class ApplicationFX extends Application {
 
+    private ConfigurableApplicationContext context;//on a besoin de connaitre le contexte spring pour pouvoir le fermer
+
+    @Override
+    public void init() {
+        String[] args = getParameters().getRaw().toArray(new String[0]);//les paramètres reçus dans le main.
+
+        // Création explicite du contexte de l'application
+        this.context = new SpringApplicationBuilder().sources(ApplicationSpring.class).run(args);
+    }
+
+    @Override
+    public void stop() {
+        // Destruction explicite du contexte
+        this.context.close();
+
+        // Fermeture de JavaFX
+        Platform.exit();
+    }
+
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         System.out.println("JavaFX initialisé");
