ae54cb73628124dabe66bd23cfe7dd53543b81c1
MabJo <1931631@cegeplimoilou.ca>
2023-09-18 20:45:09 -0400

    form2b du prof

---
+++ controleur/src/main/java/a23/climoilou/mono2/formatifs/formatif2b/controleurFX/PrototypeObserver.java
@@ -0,0 +1,2 @@
+package a23.climoilou.mono2.formatifs.formatif2b.controleurFX;public interface PrototypeObserver {
+}
---
+++ modele/src/main/java/a23/climoilou/mono2/formatifs/artistesSupplementaires/PrototypeEvent.java
@@ -0,0 +1,5 @@
+package a23.climoilou.mono2.formatifs.formatif2b.controleurFX;
+
+public interface PrototypeEvent {
+
+}
